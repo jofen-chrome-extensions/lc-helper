4676c1e10ef5e9ea0f9ab4530d741d90ea682616
Jiaofen Xu <jiaofen.xu@gmail.com>
2024-12-12 21:54:24 -0800

    Migrate to Manifest V3

--- common.js
+++
@@ -1,6 +0,0 @@
-
-function googleSearch(text, tab) {
-	let url =
-    'https://google.com/search?q=leetcode solution ' + text;
-  chrome.tabs.create({url: url, index: tab.index + 1});
-}
--- service_worker.js
+++ service_worker.js
@@ -1,3 +1,9 @@
+
+function googleSearch(text, tab) {
+	let url =
+    'https://google.com/search?q=leetcode solution ' + text;
+  chrome.tabs.create({url: url, index: tab.index + 1});
+}
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
